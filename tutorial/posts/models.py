a62339b4e4b6d72aa387d0e23ca3f830b15ee2aa
Swathy Anand <swathy.anand2000@gmail.com>
2021-09-14 12:50:44 +0530

    Setting model

--- tutorial/posts/models.py
+++ tutorial/posts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    created = models.DateTimeField(auto_now_add = True)
+    title = models.CharField(max_length = 100, blank = False)
+    content = models.TextField()
+    author = models.CharField(max_length = 100, blank = False)
+
+    class Meta:
+        ordering = ['created']
+
